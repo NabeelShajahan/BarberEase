09e998ec8fa53d78ea2a0500a8f728bfc9b6ad22
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-07-26 20:05:47 -0400

    mplemented Parcelable interface in both classes to allow passing them through Intent.

--- app/src/main/java/com/example/barberease/Barber.java
+++ app/src/main/java/com/example/barberease/Barber.java
@@ -1,6 +1,9 @@
 package com.example.barberease;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Barber {
+
+public class Barber implements Parcelable{
     private String userId;
     private String imageUrl;
     private String name;
@@ -30,6 +33,31 @@ public class Barber {
         this.coloring = coloring;
         this.styling = styling;
     }
+    protected Barber(Parcel in) {
+        userId = in.readString();
+        imageUrl = in.readString();
+        name = in.readString();
+        details = in.readString();
+        price = in.readString();
+        schedule = in.readString();
+        payments = in.readString();
+        haircut = in.readByte() != 0;
+        shave = in.readByte() != 0;
+        coloring = in.readByte() != 0;
+        styling = in.readByte() != 0;
+    }
+
+    public static final Creator<Barber> CREATOR = new Creator<Barber>() {
+        @Override
+        public Barber createFromParcel(Parcel in) {
+            return new Barber(in);
+        }
+
+        @Override
+        public Barber[] newArray(int size) {
+            return new Barber[size];
+        }
+    };
 
     // Getter and Setter methods for all fields
 
@@ -119,5 +147,24 @@ public class Barber {
 
     public void setStyling(boolean styling) {
         this.styling = styling;
+    }
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(userId);
+        parcel.writeString(imageUrl);
+        parcel.writeString(name);
+        parcel.writeString(details);
+        parcel.writeString(price);
+        parcel.writeString(schedule);
+        parcel.writeString(payments);
+        parcel.writeByte((byte) (haircut ? 1 : 0));
+        parcel.writeByte((byte) (shave ? 1 : 0));
+        parcel.writeByte((byte) (coloring ? 1 : 0));
+        parcel.writeByte((byte) (styling ? 1 : 0));
     }
 }
