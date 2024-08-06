d83022ac0ce86196b578ec9dd819285902ebb110
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-08-06 06:03:51 -0400

    created a BusinessAccountPagerAdapter for the fragments

---
+++ app/src/main/java/com/example/barberease/BusinessAccountPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.barberease;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class BusinessAccountPagerAdapter extends FragmentPagerAdapter {
+
+    public BusinessAccountPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new ReviewsFragment();
+            case 2:
+                return new ServicesFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "HOME";
+            case 1:
+                return "REVIEWS";
+            case 2:
+                return "SERVICES";
+            default:
+                return null;
+        }
+    }
+}
