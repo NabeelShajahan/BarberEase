8ceaa31793c6621d682363c12e59a390d1f96082
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-08-09 09:42:33 -0400

    direct mainactivity without logging in

--- app/src/main/java/com/example/barberease/SplashActivity.java
+++ app/src/main/java/com/example/barberease/SplashActivity.java
@@ -1,26 +1,38 @@
 package com.example.barberease;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    // Navigate to LoginActivity after the splash screen duration
-                    Intent intent = new Intent(SplashActivity.this,loginActivity.class);
+        mAuth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser != null) {
+                    // User is signed in, navigate to MainActivity
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
-                    finish();
+                } else {
+                    // No user is signed in, navigate to loginActivity
+                    Intent intent = new Intent(SplashActivity.this, loginActivity.class);
+                    startActivity(intent);
                 }
-            }, 3000);
+                finish();
+            }
+        }, 3000);
     }
 }
