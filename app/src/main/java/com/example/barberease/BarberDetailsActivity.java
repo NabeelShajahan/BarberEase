87c51132d199943bc2e93e7038f4823ebd4cc1f5
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2024-08-09 09:46:05 -0400

    similar changes to their xml and sorted out errors in connecting via firebase

--- app/src/main/java/com/example/barberease/BarberDetailsActivity.java
+++ app/src/main/java/com/example/barberease/BarberDetailsActivity.java
@@ -4,73 +4,80 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class BarberDetailsActivity extends AppCompatActivity {
 
-    private ImageView barberImage;
-    private TextView barberName, barberDescription, barberAvailability, barberServices, barberPayments;
-    private DatabaseReference barberRef;
+    private static final String TAG = "BarberDetailsActivity";
+
+    private TextView barberNameTextView, barberPhoneTextView, barberAddressTextView;
+    private ImageView barberImageView;
+    private DatabaseReference databaseReference;
+    private String barberId;
+    private String barberImageUrl;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber_details);
 
-        barberImage = findViewById(R.id.barberImage);
-        barberName = findViewById(R.id.barberName);
-        barberDescription = findViewById(R.id.barberDescription);
-        barberAvailability = findViewById(R.id.barberAvailability);
-        barberServices = findViewById(R.id.barberServices);
-        barberPayments = findViewById(R.id.barberPayments);
+        barberNameTextView = findViewById(R.id.barber_name);
+        barberPhoneTextView = findViewById(R.id.barber_phone);
+        barberAddressTextView = findViewById(R.id.barber_address);
+        barberImageView = findViewById(R.id.barber_image);
 
-        String barberId = getIntent().getStringExtra("barberId");
-        Log.d("BarberDetailsActivity", "Received barberId: " + barberId);
-        if (barberId == null) {
-            Log.e("BarberDetailsActivity", "barberId is null");
-            // You can show an error message or finish the activity here
-            finish();
-            return;
+        databaseReference = FirebaseDatabase.getInstance().getReference("barbers");
+        barberId = getIntent().getStringExtra("barberId");
+        barberImageUrl = getIntent().getStringExtra("barberImageUrl");
+
+        if (barberImageUrl != null && !barberImageUrl.isEmpty()) {
+            Picasso.get().load(barberImageUrl).into(barberImageView);
+        } else {
+            barberImageView.setImageResource(R.drawable.ic_profile);
         }
 
-        barberRef = FirebaseDatabase.getInstance().getReference("barbers").child(barberId);
+        fetchBarberDetails();
+    }
 
-        barberRef.addValueEventListener(new ValueEventListener() {
+    private void fetchBarberDetails() {
+        databaseReference.child(barberId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Barber barber = dataSnapshot.getValue(Barber.class);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Barber barber = snapshot.getValue(Barber.class);
                 if (barber != null) {
-                    Glide.with(BarberDetailsActivity.this).load(barber.getImageUrl()).into(barberImage);
-                    barberName.setText(barber.getName());
-                    barberDescription.setText(barber.getDetails());
-                    barberAvailability.setText(barber.getSchedule());
-                    barberServices.setText(getServicesString(barber));
-                    barberPayments.setText(barber.getPayments());
+                    displayBarberDetails(barber);
+                } else {
+                    Log.e(TAG, "Barber is null for snapshot: " + snapshot.toString());
                 }
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.e("BarberDetailsActivity", "DatabaseError: " + databaseError.getMessage());
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Database error: " + error.getMessage());
             }
         });
     }
 
-    private String getServicesString(Barber barber) {
-        StringBuilder services = new StringBuilder();
-        if (barber.isHaircut()) services.append("Haircut\n");
-        if (barber.isShave()) services.append("Shaving\n");
-        if (barber.isColoring()) services.append("Coloring\n");
-        if (barber.isStyling()) services.append("Styling\n");
-        return services.toString().trim();
+    private void displayBarberDetails(Barber barber) {
+        barberNameTextView.setText(barber.getName());
+        barberPhoneTextView.setText(barber.getPhone());
+
+        String address = String.format("%s, %s, %s, %s, %s, %s, %s",
+                barber.getStreetAddress(),
+                barber.getBuildingFloor(),
+                barber.getCity(),
+                barber.getStateRegion(),
+                barber.getZipCode(),
+                barber.getCountry(),
+                barber.getLocationType());
+
+        barberAddressTextView.setText(address);
     }
 }
